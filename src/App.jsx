283c278954aa310ade0009895c7ccbf59ec1e986
Potat369 <yevheniidemian@gmail.com>
2025-04-16 09:59:05 +0300

    Restructure app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import Weather from "./components/Weather.jsx";
-
-const mockData = {
-  condition: "OVERCAST",
-  humidity: 62,
-  pressure: 756,
-  c: 15,
-  f: 59,
-  windSpeed: 4,
-  windDirection: "SOUTH",
-  dewPoint: 7.7,
-  feelsLike: 12,
-  uv: 4,
-  icon: "https://yastatic.net/weather/i/icons/confident/dark/svg/ovc.svg",
-  pollution: {
-    aqi: 37,
-  },
-};
+import { useState } from "react";
+import { mockData } from "./mockData.js";
 
 export default function App() {
+  const [data, setData] = useState(mockData);
+  const [isCelsius, setIsCelsius] = useState(
+    localStorage.getItem("isCelsius") | true,
+  );
+
   return (
     <>
-      <Weather data={mockData} />
+      <main>
+        <Weather data={data} isCelsius={isCelsius} />
+      </main>
     </>
   );
 }

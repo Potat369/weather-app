fd28af8d7294cd62118c1190d0720dfe7b42cfc0
Potat369 <yevheniidemian@gmail.com>
2025-04-16 10:02:56 +0300

    Restructure weather component to use new api

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -9,18 +9,27 @@ import {
   faWind,
 } from "@fortawesome/free-solid-svg-icons";
 
-export default function Weather({ data }) {
+export default function Weather({ data, isCelsius }) {
+  const degree = isCelsius ? "C" : "F";
+
   return (
     <>
       <section className="box">
         <div className="main">
           <div>
-            <div className="temp">{data.c}°C</div>
-            <div className="prec">Cloudy</div>
-            <div className="like">Feels like {data.feelsLike}°C</div>
+            <div className="prec">{data.location.name}</div>
+            <div className="temp">
+              {isCelsius ? data.current.temp_c : data.current.temp_f}°{degree}
+            </div>
+            <div className="prec">{data.current.condition.text}</div>
+            <div className="like">
+              Feels like{" "}
+              {isCelsius ? data.current.feelslike_c : data.current.feelslike_f}°
+              {degree}
+            </div>
           </div>
           <div>
-            <img src={data.icon} className="icon" />
+            <FontAwesomeIcon icon={faSun} />
           </div>
         </div>
       </section>
@@ -30,37 +39,40 @@ export default function Weather({ data }) {
             <div>
               <FontAwesomeIcon icon={faDroplet} /> Humidity
             </div>
-            <div>{data.humidity}%</div>
+            <div>{data.current.humidity}%</div>
           </div>
           <div>
             <div>
               <FontAwesomeIcon icon={faCompress} /> Pressure
             </div>
-            <div>{data.pressure} mb</div>
+            <div>{data.current.pressure_mb} mb</div>
           </div>
           <div>
             <div>
               <FontAwesomeIcon icon={faWind} /> Wind Speed
             </div>
-            <div>{data.windSpeed} km/h</div>
+            <div>{data.current.wind_kph} km/h</div>
           </div>
           <div>
             <div>
               <FontAwesomeIcon icon={faCircleNotch} /> Dew Point
             </div>
-            <div>{data.dewPoint}°</div>
+            <div>
+              {isCelsius ? data.current.dewpoint_c : data.current.dewpoint_f}°
+              {degree}
+            </div>
           </div>
           <div>
             <div>
               <FontAwesomeIcon icon={faSun} /> UV Index
             </div>
-            <div>{data.uv} of 11</div>
+            <div>{data.current.uv} of 11</div>
           </div>
           <div>
             <div>
               <FontAwesomeIcon icon={faVirus} /> AQI
             </div>
-            <div>{data.pollution.aqi}</div>
+            <div>{data.current.air_quality.pm10}</div>
           </div>
         </div>
       </section>
